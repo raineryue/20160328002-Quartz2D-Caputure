2d03a3ebf3257088a23df10f3253b25ef6588376
raineryue <it_jinlei.yue@163.com>
2016-03-28 14:25:06 +0800

    Initial Commit

---
+++ 20160328002-Quartz2D-Caputure/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20160328002-Quartz2D-Caputure
+//
+//  Created by Rainer on 16/3/28.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 20160328002-Quartz2D-Caputure/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  20160328002-Quartz2D-Caputure
+//
+//  Created by Rainer on 16/3/28.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
